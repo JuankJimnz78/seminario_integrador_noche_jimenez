331f60613692830b0b328a043f009f2547634b82
juankjimnz78 <juankjimnz78@gmail.com>
2026-04-29 20:23:49 -0500

    29-04-2026

---
+++ modulo1/python/practica2.py
@@ -0,0 +1,7 @@
+nombre=input("Ingresa tu nombre")
+peso=input("Ingresa tu peso en kg: ")
+altura= input("Ingresa tu altura en metros:")
+
+print("Nombre de paciente:", nombre)
+print("Peso del paciente:", peso, "kg")
+print("La altura del paciente es:", altura, "m")
