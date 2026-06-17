d5e9cabb2bb9de02ad14a89c2163e234df48f715
juankjimnz78 <juankjimnz78@gmail.com>
2026-06-17 14:58:57 -0500

    Clase tipeScript

---
+++ modulo3/typeScript/03_presentacion.ts
@@ -0,0 +1,5 @@
+const nombre: string  = "Ana Garcia";
+const edad:   number  = 28;
+const ciudad1: string = "Madrid";
+
+console.log(`El nombre es: ${nombre}, tiene: ${edad} años de edad, ciudad ${ciudad1}`);
---
+++ modulo3/typeScript/12_condicional_if.ts
@@ -0,0 +1,26 @@
+console.log ("Condicional if simple");
+
+const edad: number = 19;
+
+if (edad >= 18){
+    console.log("Es mayor de edad");
+}
+
+console.log("Condicional if dos caminos");
+
+
+
+if (tieneLicencia =="si"){
+    console.log("Puede manejar");
+}else{
+    console.log("No puede manejara");
+}
+
+console.log("Condicional if multiple");
+type 
+
+let destinoInternacional: boolean =
+
+
+
+
